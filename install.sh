eef6e2ecbb8366e080af789fc59f5a9b5e32382e
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-07-27 14:45:01 -0400

    (FG): added installer script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ~/explorer_bot/src
+cd ~/explorer_bot/src
+vcs import < ~/current_state.repos
+cd ~/explorer_bot
+source /opt/ros/jazzy/setup.bash
+rosdep install -i --from-paths src --rosdistro jazzy -y
+colcon build --symlink-install
