e7de389b78a9c2cbdbf01ca17c5cc22a921a9808
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-07-29 11:40:11 -0400

    (FG): modified install script

--- install.sh
+++ install.sh
@@ -8,4 +8,10 @@ vcs import < ~/current_state.repos
 cd ~/explorer_bot
 source /opt/ros/jazzy/setup.bash
 rosdep install -i --from-paths src --rosdistro jazzy -y
+cd ~/explorer_bot/src/gz_worlds_and_maps/world/src/hospital
+unzip models_part1.zip
+unzip models_part2.zip
+unzip models_part3.zip
+unzip models_part4.zip
 colcon build --symlink-install
+source ~/explorer_bot/install/setup.bash
